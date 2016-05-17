edb697ca5caa55c604f64d690fb30dc252a30841
Nicky Semenza <nicky@nickysemenza.com>
2016-05-16 18:59:04 -0700

    updated library to support listen for polling + responding to polling

--- Artnet.cpp
+++ Artnet.cpp
@@ -32,10 +32,16 @@ void Artnet::begin(byte mac[], byte ip[])
   Udp.begin(ART_NET_PORT);
 }
 
+void Artnet::setBroadcast(byte bc[])
+{
+  //sets the broadcast address
+  broadcast = bc;
+}
+
 uint16_t Artnet::read()
 {
   packetSize = Udp.parsePacket();
-  
+  IPAddress artnetServer = Udp.remoteIP();
   if (packetSize <= MAX_BUFFER_ARTNET && packetSize > 0)
   { 
       Udp.read(artnetPacket, MAX_BUFFER_ARTNET);
@@ -46,7 +52,6 @@ uint16_t Artnet::read()
         if (artnetPacket[i] != ART_NET_ID[i])
           return 0;
       }
-        
       opcode = artnetPacket[8] | artnetPacket[9] << 8; 
 
       if (opcode == ART_DMX)
@@ -60,6 +65,72 @@ uint16_t Artnet::read()
       }
       if (opcode == ART_POLL)
       {
+        //fill the reply struct, and then send it to the network's broadcast address
+        Serial.print("POLL from ");
+        Serial.print(artnetServer);
+        Serial.print(" broadcast addr: ");
+        Serial.println(broadcast);
+
+        IPAddress local_ip = Ethernet.localIP();
+        node_ip_address[0] = local_ip[0];
+        node_ip_address[1] = local_ip[1];
+        node_ip_address[2] = local_ip[2];
+        node_ip_address[3] = local_ip[3];
+
+        sprintf((char *)id, "Art-Net\0");
+        memcpy(ArtPollReply.id, id, sizeof(ArtPollReply.id));
+        memcpy(ArtPollReply.ip, node_ip_address, sizeof(ArtPollReply.ip));
+
+        ArtPollReply.opCode = ART_POLL_REPLY;
+        ArtPollReply.port =  ART_NET_PORT;
+        
+        memset(ArtPollReply.goodinput,  0x08, 4);
+        memset(ArtPollReply.goodoutput,  0x80, 4);
+        memset(ArtPollReply.porttypes,  0xc0, 4);
+
+        uint8_t shortname [18];
+        uint8_t longname [64];
+        sprintf((char *)shortname, "artnet arduino\0");
+        sprintf((char *)longname, "Art-Net -> Arduino Bridge\0");
+        memcpy(ArtPollReply.shortname, shortname, sizeof(shortname));
+        memcpy(ArtPollReply.longname, longname, sizeof(longname));
+
+        ArtPollReply.etsaman[0] = 0;
+        ArtPollReply.etsaman[1] = 0;
+        ArtPollReply.verH       = 1;
+        ArtPollReply.ver        = 0;
+        ArtPollReply.subH       = 0;
+        ArtPollReply.sub        = 0;
+        ArtPollReply.oemH       = 0;
+        ArtPollReply.oem        = 0xFF;
+        ArtPollReply.ubea       = 0;
+        ArtPollReply.status     = 0xd2;
+        ArtPollReply.swvideo    = 0;
+        ArtPollReply.swmacro    = 0;
+        ArtPollReply.swremote   = 0;
+        ArtPollReply.style      = 0;   
+
+        ArtPollReply.numbportsH = 0;
+        ArtPollReply.numbports  = 4;
+        ArtPollReply.status2    = 0x08;
+
+        ArtPollReply.bindip[0] = node_ip_address[0];
+        ArtPollReply.bindip[1] = node_ip_address[1];
+        ArtPollReply.bindip[2] = node_ip_address[2];
+        ArtPollReply.bindip[3] = node_ip_address[3];
+
+        uint8_t swin[4]  = {0x01,0x02,0x03,0x04};
+        uint8_t swout[4] = {0x01,0x02,0x03,0x04};
+        for(uint8_t i = 0; i <= 4; i++)
+        {
+            ArtPollReply.swout[i] = swout[i];
+            ArtPollReply.swin[i] = swin[i];
+        }
+        sprintf((char *)ArtPollReply.nodereport, "%i DMX output universes active.\0", ArtPollReply.numbports);  
+        Udp.beginPacket(broadcast, ART_NET_PORT);//send the packet to the broadcast address
+        Udp.write((uint8_t *)&ArtPollReply, sizeof(ArtPollReply));
+        Udp.endPacket();
+
         return ART_POLL; 
       }
   }
--- Artnet.h
+++ Artnet.h
@@ -33,6 +33,7 @@ THE SOFTWARE.
 #define ART_NET_PORT 6454
 // Opcodes
 #define ART_POLL 0x2000
+#define ART_POLL_REPLY 0x2100
 #define ART_DMX 0x5000
 // Buffers
 #define MAX_BUFFER_ARTNET 530
@@ -40,12 +41,51 @@ THE SOFTWARE.
 #define ART_NET_ID "Art-Net\0"
 #define ART_DMX_START 18
 
+struct artnet_reply_s {
+  uint8_t  id[8];
+  uint16_t opCode;
+  uint8_t  ip[4];
+  uint16_t port;
+  uint8_t  verH;
+  uint8_t  ver;
+  uint8_t  subH;
+  uint8_t  sub;
+  uint8_t  oemH;
+  uint8_t  oem;
+  uint8_t  ubea;
+  uint8_t  status;
+  uint8_t  etsaman[2];
+  uint8_t  shortname[18];
+  uint8_t  longname[64];
+  uint8_t  nodereport[64];
+  uint8_t  numbportsH;
+  uint8_t  numbports;
+  uint8_t  porttypes[4];//max of 4 ports per node
+  uint8_t  goodinput[4];
+  uint8_t  goodoutput[4];
+  uint8_t  swin[4];
+  uint8_t  swout[4];
+  uint8_t  swvideo;
+  uint8_t  swmacro;
+  uint8_t  swremote;
+  uint8_t  sp1;
+  uint8_t  sp2;
+  uint8_t  sp3;
+  uint8_t  style;
+  uint8_t  mac[6];
+  uint8_t  bindip[4];
+  uint8_t  bindindex;
+  uint8_t  status2;
+  uint8_t  filler[26];
+} __attribute__((packed));
+
 class Artnet
 {
 public:
   Artnet();
 
   void begin(byte mac[], byte ip[]);
+  void setBroadcast(byte bc[]);
   uint16_t read();
   void printPacketHeader();
   void printPacketContent();
@@ -82,10 +122,13 @@ public:
   }
 
 private:
+  uint8_t  node_ip_address[4];
+  uint8_t  id[8];
   EthernetUDP Udp;
-  
+  struct artnet_reply_s ArtPollReply;
   uint8_t artnetPacket[MAX_BUFFER_ARTNET];
   uint16_t packetSize;
+  IPAddress broadcast;
   uint16_t opcode;
   uint8_t sequence;
   uint16_t incomingUniverse;
