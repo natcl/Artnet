06f464a64343a2f04dfa5287a7c94eb8d2976dac
Nathanaël Lécaudé <natcl@users.noreply.github.com>
2018-03-06 11:45:48 -0500

    Typo

--- examples/OctoWS2811/ArtnetOctoWS2811Sync/ArtnetOctoWS2811Sync.ino
+++ examples/OctoWS2811/ArtnetOctoWS2811Sync/ArtnetOctoWS2811Sync.ino
@@ -60,7 +60,7 @@ void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t* d
   previousDataLength = length;
 }
 
-void onSync(IPAdress remoteIP) {
+void onSync(IPAddress remoteIP) {
     leds.show();
 }
 
